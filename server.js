dd5dfc3fa8779bee6735b1038b64745d63d9761a
Ziniya Ashish <ashishziniya6@gmail.com>
2025-01-18 14:08:35 +0000

    update

--- server.js
+++ server.js
@@ -27,8 +27,20 @@ app.use(passport.session());
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
+const LocalStrategy = require('passport-local');
+
 myDB(async (client) => {
   const myDataBase = await client.db('test');
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user);
+    });
+  }));
 
   app.route('/').get((req, res) => {
     res.render('index', {
@@ -55,6 +67,37 @@ myDB(async (client) => {
   console.error('Database connection failed:', e);
   app.route('/').get((req, res) => {
     res.render('index', { title: e.message, message: 'Unable to connect to database' });
+  });
+});
+
+app.route('/').get((req, res) => {
+  res.render('index', {
+    title: 'Connected to Database',
+    message: 'Please login',
+    showLogin: true
+  });
+});
+
+app.route('/login').post(
+  passport.authenticate('local', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/profile');
+  }
+);
+
+// Middleware to ensure authentication
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
+
+// Protected profile route
+app.route('/profile').get(ensureAuthenticated, (req, res) => {
+  res.render('profile', {
+    title: 'Profile Page',
+    user: req.user
   });
 });
 
